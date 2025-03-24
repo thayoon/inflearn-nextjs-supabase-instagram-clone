6d5338edcbba5a6c01bee8e615045c5dd93a5a9b
thayoon <thagyun@naver.com>
2025-03-25 01:26:25 +0900

    feat: email verifyOTP signUp, signin

--- components/auth/signin.tsx
+++ components/auth/signin.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import { Button, Input } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function SignIn({ setView }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const supabase = createBrowserSupabaseClient();
+  const signInMutation = useMutation({
+    mutationFn: async () => {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (data) {
+        console.log(data);
+      }
+
+      if (error) {
+        alert(error.message);
+      }
+    },
+  });
   return (
     <div className="flex flex-col gap-4">
       <div className="gap-2 pt-10 pb-6 px-10 w-full flex flex-col items-center justify-center max-w-lg border border-gray-400 bg-white">
@@ -28,8 +49,10 @@ export default function SignIn({ setView }) {
         />
         <Button
           onClick={() => {
-            console.log("signin");
+            signInMutation.mutate();
           }}
+          loading={signInMutation.isPending}
+          disabled={signInMutation.isPending}
           color="light-blue"
           className="w-full text-md py-1"
         >
